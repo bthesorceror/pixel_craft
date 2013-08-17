65e0e493d2aabf7895eb4e2b5b87de807766a872
bthesorceror <bthesorceror@gmail.com>
2013-08-16 23:58:00 -0400

    Setting up basic web server

---
+++ index.js
@@ -0,0 +1,13 @@
+var Journeyman      = require('journeyman');
+var Rudder          = require('rudder');
+var LightningStrike = require('lightning_strike');
+var path            = require('path');
+
+var journeyman = new Journeyman(3000);
+var rudder = new Rudder();
+var strike = new LightningStrike(path.join(__dirname, 'static'));
+
+journeyman.use(rudder.middleware());
+journeyman.use(strike.middleware());
+
+journeyman.listen();
